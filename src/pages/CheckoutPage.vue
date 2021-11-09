8ea2f18e00c2c01fdfa604387adef3d6e2b3426e
errea <erreakezy@gmail.com>
2021-11-09 12:49:32 +0100

    Add CheckoutPage file

---
+++ src/pages/CheckoutPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div :class="isCard ? '' : 'lg:h-screen'"
+    class="container mx-auto p-6 grid grid-cols-1
+    row-gap-12 lg:grid-cols-10 lg:col-gap-10 lg:pt-12">
+
+    </div>
+</template>
+
+<script>
+
+</script>
