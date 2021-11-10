a640025af7102afc85549104801cb065a456a7ee
errea <erreakezy@gmail.com>
2021-11-10 23:37:31 +0100

    Modify main.js + add vue class instance

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import { createApp } from 'vue';
+import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+// import router from './router';
+// import store from './store';
 import './assets/css/main.css';
 
-createApp(App).use(store).use(router).mount('#app');
+// createApp(App).use(store).use(router).mount('#app');
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
