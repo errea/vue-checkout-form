707ff991d8f2b17472cb9211046186533c209fb6
errea <erreakezy@gmail.com>
2021-11-11 00:08:37 +0100

    Modify dependencies

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
+
 // import router from './router';
 // import store from './store';
 import './assets/css/main.css';
 
+import './assets/tailwind.css';
+
 // createApp(App).use(store).use(router).mount('#app');
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
