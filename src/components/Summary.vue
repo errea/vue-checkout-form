d26799e039b9bddc5e966254b8742fda07100204
errea <erreakezy@gmail.com>
2021-11-12 17:01:08 +0100

    Add Summary.vue file

---
+++ src/components/Summary.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Summary',
+    props: {
+        items: object
+    },
+    component:{
+        items
+    },
+    data () {
+        return {
+            
+        }
+    }
+};
+
+</script>
